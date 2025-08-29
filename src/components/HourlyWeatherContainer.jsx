70f4198ece70550982b66dd03b41066f2fae4351
ADHARSH KOLLI <160266525+Adharsh798@users.noreply.github.com>
2025-08-29 10:23:23 +0530

    Update HourlyWeatherContainer.jsx

--- src/components/HourlyWeatherContainer.jsx
+++ src/components/HourlyWeatherContainer.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import HourCard from "./HourCard";
-
-// function HourlyWeatherContainer({ hourlyData }) {
-//   return (
-//     <div className="hourly-container">
-//       {hourlyData.map((hour, idx) => (
-//         <HourCard key={idx} data={hour} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default HourlyWeatherContainer;
-// import React from "react";
-// import HourCard from "./HourCard";
-
-// function HourlyWeatherContainer({ hourlyData }) {
-//   return (
-//     <div className="hourly-container">
-//       {/* ✅ Heading inside the box, above cards */}
-//       <h3 className="hourly-heading">Hourly Weather</h3>
-
-//       {/* ✅ Scrollable row of hour cards */}
-//       <div className="hourly-scroll">
-//         {hourlyData.map((hour, idx) => (
-//           <HourCard key={idx} data={hour} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HourlyWeatherContainer;
 import React from "react";
 import HourCard from "./HourCard";
 
